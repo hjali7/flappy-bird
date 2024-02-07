25709a6ce366d7786ca51244653d05280b843fe1
hjali7 <hjikingali@gmail.com>
2024-02-07 15:15:28 +0330

    setup for project

---
+++ index.js
@@ -0,0 +1,20 @@
+const board = document.getElementById("board");
+const context = board.getContext("2d");
+
+const birdWidth = 34 ;
+const birdHeight = 24 ;
+
+const birdX = birdWidth / 8 ;
+const birdY = birdHeight / 2 ;
+
+const bird = {
+    x : birdX ,
+    y : birdY ,
+    width : birdWidth ,
+    height : birdHeight,
+};
+
+window.onload = () => {
+    
+}
+
